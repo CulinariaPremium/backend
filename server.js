f274e0dbce93ef22f693e0c1ee3400e6e311751f
CulináriaPremium <contatoculinariapremium@gmail.com>
2024-12-03 23:54:54 -0300

    Atualizando backend com novos recursos

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const pedidos = {
     "123456789": { status: "Entregue", detalhes: "Pedido entregue em 01/12/2024." },
     "987654321": { status: "Em transporte", detalhes: "Pedido a caminho do destino." },
     "111222333": { status: "Aguardando envio", detalhes: "Pedido ainda não foi despachado." }
+    "PPCPMTB5FPHFCK": { status: "Aguardando envio", detalhes: "Pedido ainda não foi despachado." }
 };
 
 // Rota para rastrear pedido
